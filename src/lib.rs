7e323d0697f914a0f680786d8639b7862f9b4408
Olivier 'reivilibre <git.oo@emunest.net>
2026-06-14 18:10:26 +0100

    Reformat TypeIdHasher code to be clearer with if let
    Signed-off-by: Olivier 'reivilibre <git.oo@emunest.net>

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@
 #![warn(missing_docs, unused_results)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use core::convert::TryInto;
+use core::convert::TryFrom;
 use core::hash::Hasher;
 
 #[cfg(not(feature = "std"))]
@@ -625,9 +625,10 @@ impl Hasher for TypeIdHasher {
         // contract for safety. But I’m OK with release builds putting everything in one bucket
         // if it *did* change (and debug builds panicking).
         debug_assert_eq!(bytes.len(), 8);
-        let _ = bytes
-            .try_into()
-            .map(|array| self.value = u64::from_ne_bytes(array));
+
+        if let Ok(array) = <[u8; 8]>::try_from(bytes) {
+            self.value = u64::from_ne_bytes(array);
+        }
     }
 
     #[inline]
